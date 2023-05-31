64cc3ffff58b6a29525f206a492a5a643f86d1dd
maelissteissier <maeliss.teissier@gmail.com>
2023-05-31 15:48:21 -0400

    edit LOCAL_PROD react port

--- journailing-frontend/LOCAL_PROD_launch_frontend.sh
+++ journailing-frontend/LOCAL_PROD_launch_frontend.sh
@@ -1,1 +1,2 @@
-REACT_APP_BACKEND_URL='http://192.168.2.31:9876' HOST=0.0.0.0 npm run start
+webpack --config webpack.config.js
+PORT=9875  REACT_APP_BACKEND_URL='http://192.168.2.31:9876' HOST=0.0.0.0 npm run start

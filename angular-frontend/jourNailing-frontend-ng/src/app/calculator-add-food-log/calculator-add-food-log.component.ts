3e85b2db80542f63136f39f5e73a404d588179df
maelissteissier <maeliss.teissier@gmail.com>
2023-06-06 21:37:16 -0400

    add food journal entry form

--- angular-frontend/jourNailing-frontend-ng/src/app/calculator-add-food-log/calculator-add-food-log.component.ts
+++ angular-frontend/jourNailing-frontend-ng/src/app/calculator-add-food-log/calculator-add-food-log.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+import DateUtils from "../shared/DateUtils";
 
 @Component({
-  selector: 'app-calculator-add-food-log',
-  templateUrl: './calculator-add-food-log.component.html',
-  styleUrls: ['./calculator-add-food-log.component.scss']
+    selector: 'app-calculator-add-food-log',
+    templateUrl: './calculator-add-food-log.component.html',
+    styleUrls: ['./calculator-add-food-log.component.scss']
 })
 export class CalculatorAddFoodLogComponent {
+    now = new Date();
+    day = DateUtils.getDateStringFromDatetime(this.now);
+    time = DateUtils.getTimeStringFromDatetime(this.now);
 
 }
--- angular-frontend/jourNailing-frontend-ng/src/app/calories-calculator/calories-calculator.component.ts
+++ angular-frontend/jourNailing-frontend-ng/src/app/calories-calculator/calories-calculator.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {faFloppyDisk} from "@fortawesome/free-solid-svg-icons";
+import {faFloppyDisk as farFloppyDisk} from "@fortawesome/free-regular-svg-icons";
 
 @Component({
-  selector: 'app-calories-calculator',
-  templateUrl: './calories-calculator.component.html',
-  styleUrls: ['./calories-calculator.component.scss']
+    selector: 'app-calories-calculator',
+    templateUrl: './calories-calculator.component.html',
+    styleUrls: ['./calories-calculator.component.scss']
 })
 export class CaloriesCalculatorComponent {
-
+    faFloppyDisk = faFloppyDisk;
+    farFloppyDisk = farFloppyDisk;
 }

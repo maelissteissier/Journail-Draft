43b2ce90462af4ca07f85339074af2f513de09da
maelissteissier <maeliss.teissier@gmail.com>
2023-07-20 14:51:42 -0400

    add food journal display

--- angular-frontend/jourNailing-frontend-ng/src/app/shared/services/food-journal-entry.service.ts
+++ angular-frontend/jourNailing-frontend-ng/src/app/shared/services/food-journal-entry.service.ts
@@ -2,6 +2,7 @@ import {HttpClient} from '@angular/common/http';
 import {FoodJournalEntry} from "../models/food-journal-entry";
 import {Injectable} from '@angular/core';
 import {environment} from "../../../environments/environment";
+import DateUtils from "../DateUtils";
 
 @Injectable({
     providedIn: 'root'
@@ -18,6 +19,13 @@ export class FoodJournalEntryService {
 
     saveFoodEntry(foodEntryData: FoodJournalEntry) {
         return this.http.post(`${this.baseURL}/food-journal-entry`, foodEntryData);
+    }
+
+    fetchFoodJournalEntries(date: Date){
+        const formattedDate = DateUtils.getDateStringFromDatetime(date);
+        console.log(`${this.baseURL}/food-journal-entries/date?date=${formattedDate}`)
+        return this.http.get(`${this.baseURL}/food-journal-entries/date?date=${formattedDate}`)
+
     }
 }
 
